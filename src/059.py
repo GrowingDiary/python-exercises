d995c4b37cbb1c4609e26b47583181cb9d337d39
shanhui <shanhui@agora.io>
2018-10-15 22:42:22 +0800

    length of str

---
+++ src/059.py
@@ -0,0 +1,15 @@
+# !/usr/bin/python3
+# -- coding: utf-8 --
+"""
+题目：计算字符串长度。
+"""
+
+
+def main():
+    a = 'abcdefg'
+    print('字符串长度：', len(a))
+    print('字符串长度：', a.__len__())
+
+
+if __name__ == '__main__':
+    main()
